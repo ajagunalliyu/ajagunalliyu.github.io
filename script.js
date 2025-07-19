4813db17b178f6bf1663020917f3f74bc6732ed2
Ajagun Alliyu Aremu <122964975+ajagunalliyu@users.noreply.github.com>
2025-07-19 13:15:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -74,12 +74,4 @@ document.addEventListener("DOMContentLoaded", () => {
   if (yearSpan) {
     yearSpan.textContent = new Date().getFullYear();
   }
-  document.addEventListener("DOMContentLoaded", () => {
-  const menuToggle = document.getElementById("menu-toggle");
-  const mobileMenu = document.getElementById("mobile-menu");
-
-  menuToggle.addEventListener("click", () => {
-    mobileMenu.classList.toggle("active");
-  });
-});
 });
